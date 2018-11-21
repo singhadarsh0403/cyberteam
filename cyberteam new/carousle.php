007edc8f954a6d4f2616160317ee552c152574b3
Adarsh Singh <33371708+singhadarsh0403@users.noreply.github.com>
2018-11-21 14:48:08 +0530

    changes added in index.html

---
+++ cyberteam new/carousle.php
@@ -0,0 +1,40 @@
+<div class="container" id="slider"><!--container starts-->
+
+		<div class="col-md-12"><!--col-md-12 starts-->
+			<div id="myCarousel" class="carousel slide" data-ride="carousel"><!--carousle slide starts-->
+				<ol class="carousel-indicators"><!--carousle-indicator starts-->
+					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+					<li dat-target="#myCarousel" data-slide-to="1"></li>
+					<li dat-target="#myCarousel" data-slide-to="2"></li>
+					<li dat-target="#myCarousel" data-slide-to="3"></li>
+				</ol><!--carousle-indicator starts-->
+				<div class="carousel-inner"><!--carousle inner starts-->
+					<div class="item-active">
+						<img src="admin_area/slide_images/1.jpg" style="width: 100%";>
+					</div>
+					<div class="item">
+						<img src="admin_area/slide_images/2.jpg" style="width: 100%"; >
+					</div>
+					<div class="item">
+						<img src="admin_area/slide_images/3.jpg" style="width: 100%";>
+					</div>
+					<div class="item">
+						<img src="admin_area/slide_images/2.jpg" style="width: 100%";> 
+					</div>	
+				</div><!--carousle inner ends-->
+
+				<a class="left carousle-control" href="#myCarousel" data-slide="prev"><!--left carousle-control starts-->
+					<span class="glyphicon glyphicon-chevron-left"></span>
+					<span class="sr-only">Previous</span>
+				</a>
+				<a class="right carousle-control" href="#myCarousel" data-slide="next"><!--right carousle-control starts-->
+					<span class="glyphicon glyphicon-chevron-right"></span>
+					<span class="sr-only">Next</span>
+				</a><!--right carousle-control ends-->
+					
+
+			</div><!--carousle slide ends-->
+			
+		</div><!--col-md-12 Ends-->
+		
+	</div><!--container Ends-->
---
+++ cyberteam new/includes/footer.php
@@ -0,0 +1,77 @@
+<div id="footer"><!--footer starts-->
+	<div class="container"><!--container starts-->
+		<div class="row"><!--row starts-->
+			<div class="col-md-3 col-sm-6"><!--"col-md-3 col-sm-6" starts-->
+				<h4 class="xyz">Cyberteam</h4>
+				<p>"Transfroming engineering to cyberteam"</p>
+				
+				<hr>
+				<h4>Find Us on Map</h4>
+				<iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28840.073583475463!2d86.4559337461716!3d25.371008214180044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f1eee66aa3ebc7%3A0x1bcf4fdc391adc06!2sMunger%2C+Bihar+811201!5e0!3m2!1sen!2sin!4v1539762958295" width="50%" height="78" frameborder="0" style="border:0" allowfullscreen></iframe>
+				<hr class="hidden-md hidden-lg hidden-sm">
+			</div><!--"col-md-3 col-sm-6" ends-->
+
+				<div class="col-md-3 col-sm-6"><!--"col-md-3 col-sm-6" starts-->
+					<img src="img/cyberteam.png" style="width: 70px; height: 50px;"><br>
+					<p class="pqr">Cyberteam Team is modern day game changer</p>
+					
+					<hr class="hidden-md hidden-lg">
+				</div><!--"col-md-3 col-sm-6" ends-->
+				<div class="col-md-3 col-sm-6"><!--col-md-3 col-sm-6 starts-->
+					<h4>Where to find us</h4>
+					<p><!--p starts-->
+						<strong>Cyber Team.</strong>
+						<br>Mogal bazar Munger 811201
+						<br>Munger
+						<br>+917991144316
+						<br>shikharabhijeet1995@gmail.com
+						<br><strong>Abhijeet shikhar</strong>
+					</p><!--p ends-->
+					<a href="#">Go to Contact us page</a>
+					<hr class="hidden-md hidden-lg">
+				</div><!--col-md-3 col-sm-6 Ends-->
+				<div class="col-md-3 col-sm-6"><!--col-md-3 col-sm-6 starts-->	
+					<h4>Get the news</h4>
+					<p class="text-muted">
+						Few hr left 
+					</p>
+					<form action="https://feedburner.google.com/fb/a/mailverify" method="post" target="popupwindow" onsubmit="window.open('https://feedburner.google.com/fb/a/mailverify?uri=FeedbulletinForshikharabhijeet1995', 'popupwindow', 'scrollbars=yes,width=550,height=520');return true"><!--form starts-->
+						<div class="input-group"><!--input group starts-->
+							<input type="text" class="form-control" name="email">
+							<input type="hidden" name="loc" value="en_US"/>
+							<span class="input-group-btn"><!--input-group-btn starts-->
+								<input type="submit" value="subscribe" class="btn btn-default">
+							</span><!--input-group-btn ends-->
+						</div><!--input group ends-->
+					</form><!--form ends-->
+					<hr>
+					<h4>Stay in touch</h4>
+					<p class="social"><!--social starts-->
+						<a href="#"><i class="fa fa-facebook"></i></a>
+						<a href="#"><i class="fa fa-twitter"></i></a>
+						<a href="#"><i class="fa fa-instagram"></i></a>
+						<a href="#"><i class="fa fa-google-plus"></i></a>
+						<a href="#"><i class="fa fa-envelope"></i></a>
+					</p><!--social ends-->
+
+				</div><!--col-md-3 col-sm-6 Ends-->
+
+		</div><!--row ends-->
+	</div><!--container ends-->
+</div><!--footer ends-->
+
+<div id="copyright"><!--copy right starts-->
+	<div class="container"><!--container starts-->
+		<div class="col-md-6"><!--col-md-6 starts-->
+			<p class="pull-left">
+				&nbsp &nbsp&nbsp&nbsp&nbsp&nbsp &nbsp&nbsp&nbsp&nbsp&copy;2018 cyberteam
+			</p>
+		</div><!--col-md-6 ends-->
+
+		<div class="col-md-6"><!--col-md-6 starts-->
+			<p class="pull-right">
+				<a href="#">cyberteam</a>
+			</p>
+		</div><!--col-md-6 ends-->
+	</div><!--container ends-->
+</div><!--copy right ends-->
